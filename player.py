20018e5ff5880f02bfd17d9326afb643e137c578
Adam <relomy@gmail.com>
2018-10-19 22:59:40 -0400

    added WR matchup function setter and shell subclass for TE/DST

--- player.py
+++ player.py
@@ -21,12 +21,18 @@ class Player:
 
         # ECR
         self.matchup = None
-        self.rank = None
 
         # vegas
         self.overunder = None
         self.line = None
         self.projected = None
+
+        # rankings
+        # self.average_ppg = average_ppg
+        self.ecr = None
+        self.ecr_data = None
+        self.plus_minus = None
+        self.salary_rank = None
 
     def __repr__(self):
         return "Player({}, {})".format(self.name, self.rank)
@@ -84,12 +90,57 @@ class RB(Player):
         self.week_rush_atts = None
         self.week_targets = None
 
-        # rankings
-        # self.average_ppg = average_ppg
-        self.ecr = None
-        self.ecr_data = None
-        self.plus_minus = None
-        self.salary_rank = None
+    def set_dvoa_fields(self, run_dvoa, rb_pass_dvoa):
+        self.run_dvoa = run_dvoa
+        self.rb_pass_dvoa = rb_pass_dvoa
 
     def __repr__(self):
         return("RB({}, {} ({}), {})".format(self.name, self.salary, self.salary_percent, self.opp_excel))
+
+
+class WR(Player):
+    """WR subclass of Player."""
+    def __init__(self, position, player_name, team_abbv, salary, game_info, average_ppg):
+        Player.__init__(self, position, player_name, team_abbv, salary, game_info, average_ppg)
+
+        # matchup
+        self.pass_def_rank = None
+        self.wr1_rank = None
+        self.wr2_rank = None
+        self.dline = None
+
+        # season
+        self.season_snap_percent = None
+        self.season_targets = None
+        self.season_recepts = None
+
+        # last week
+        self.week_snap_percent = None
+        self.week_targets = None
+        self.week_recepts = None
+
+    def set_dvoa_fields(self, pass_def_rank, wr1_rank, wr2_rank):
+        self.pass_def_rank = pass_def_rank
+        self.wr1_rank = wr1_rank
+        self.wr2_rank = wr2_rank
+
+    def __repr__(self):
+        return("wr({}, {} ({}), {})".format(self.name, self.salary, self.salary_percent, self.opp_excel))
+
+
+class TE(Player):
+    """TE subclass of Player."""
+    def __init__(self, position, player_name, team_abbv, salary, game_info, average_ppg):
+        Player.__init__(self, position, player_name, team_abbv, salary, game_info, average_ppg)
+
+    def __repr__(self):
+        return("TE({}, {} ({}), {})".format(self.name, self.salary, self.salary_percent, self.opp_excel))
+
+
+class DST(Player):
+    """DST subclass of Player."""
+    def __init__(self, position, player_name, team_abbv, salary, game_info, average_ppg):
+        Player.__init__(self, position, player_name, team_abbv, salary, game_info, average_ppg)
+
+    def __repr__(self):
+        return("DST({}, {} ({}), {})".format(self.name, self.salary, self.salary_percent, self.opp_excel))
